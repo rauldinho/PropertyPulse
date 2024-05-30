d1d1823a6039748f9301a4aa368a62275a301051
Raul Requena <requenaraul@gmail.com>
2024-05-30 16:20:57 -0400

    38. Add Property POST API Route

---
+++ app/api/properties/route.js
@@ -0,0 +1,67 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+// GET /api/properties
+export const GET = async (request) => {
+    try {
+        await connectDB();
+
+        const properties = await Property.find({});
+
+        return new Response(JSON.stringify(properties), {
+            status: 200,
+        });
+    } catch (error) {
+        console.log(error);
+        return new Response("Something Went Wrong", { status: 500 });
+    }
+};
+
+// POST /api/properties
+export const POST = async (request) => {
+    try {
+        const formData = await request.formData();
+
+        //Access all values from amenities and images
+        const amenities = formData.getAll("amenities");
+        const images = formData
+            .getAll("images")
+            .filter((image) => image.name !== "");
+
+        //Create propertyData object for database
+        const propertyData = {
+            type: formData.get("type"),
+            name: formData.get("name"),
+            description: formData.get("description"),
+            location: {
+                street: formData.get("location.street"),
+                city: formData.get("location.city"),
+                state: formData.get("location.state"),
+                zipcode: formData.get("location.zipcode"),
+            },
+            beds: formData.get("beds"),
+            baths: formData.get("baths"),
+            square_feet: formData.get("square_feet"),
+            amenities: amenities,
+            rates: {
+                weekly: formData.get("rates.weekly"),
+                monthly: formData.get("rates.monthly"),
+                nightly: formData.get("rates.nightly"),
+            },
+            seller_info: {
+                name: formData.get("seller_info.name"),
+                email: formData.get("seller_info.email"),
+                phone: formData.get("seller_info.phone"),
+            },
+            images: images,
+        };
+
+        console.log(propertyData);
+
+        return new Response(JSON.stringify({ message: "Success" }), {
+            status: 200,
+        });
+    } catch (error) {
+        return new Response("Failed to add property", { status: 500 });
+    }
+};
---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "@/assets/styles/global.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
+
+export const metadata = {
+    title: "PropertyPulse | Find The Perfect Rental",
+    description: "Find your dream rental property",
+    keywords: "rental, find rentals, find properties",
+};
+
+const MainLayout = ({ children }) => {
+    return (
+        <AuthProvider>
+            <html lang="en">
+                <body>
+                    <Navbar />
+                    <main>{children}</main>
+                    <Footer />
+                </body>
+            </html>
+        </AuthProvider>
+    );
+};
+
+export default MainLayout;
