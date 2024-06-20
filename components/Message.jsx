182e4484ef5490403d357e87609b63364c9f38b1
Raul Requena <requenaraul@gmail.com>
2024-06-20 18:16:50 -0400

    65. Mark As Read

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const Message = ({ message }) => {
-    console.log(message);
+    const [isRead, setIsRead] = useState(true);
+
+    const handleReadClick = async () => {
+        try {
+            const res = await fetch(`/api/messages/${message._id}`, {
+                method: "PUT",
+            });
+
+            if (res.status === 200) {
+                const { read } = await res.json();
+                setIsRead(read);
+                if (read) {
+                    toast.success("Marked as read");
+                } else {
+                    toast.success("Marked as new");
+                }
+            }
+        } catch (error) {
+            console.error(error);
+            toast.error("Something went wrong");
+        }
+    };
+
     return (
         <div className="relative bg-white p-4 rounded-md shadow-md border border-gray-200">
+            {!isRead && (
+                <div className="absolute top-2 right-2 bg-yellow-500 text-white px-2">
+                    New
+                </div>
+            )}
             <h2 className="text-xl mb-4">
                 <span className="font-bold">Property Inquiry:</span>
                 {""}
@@ -36,8 +65,13 @@ const Message = ({ message }) => {
                     {new Date(message.createdAt).toLocaleString()}
                 </li>
             </ul>
-            <button className="mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md">
-                Mark As Read
+            <button
+                className={`mt-4 mr-3 ${
+                    isRead ? "bg-gray-300" : "bg-blue-500 text-white"
+                } py-1 px-3 rounded-md`}
+                onClick={handleReadClick}
+            >
+                {isRead ? "Mark as New" : "Mark as Read"}
             </button>
             <button className="mt-4 bg-red-500 text-white py-1 px-3 rounded-md">
                 Delete
