dbb32a9be681a3b08ffe747503e879b53b3b5802
Raul Requena <requenaraul@gmail.com>
2024-06-24 20:33:24 -0400

    76. Deploy to Vercel

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,8 @@ import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
 import FeaturedProperties from "@/components/FeaturedProperties";
 
+export const dynamic = "force-dynamic";
+
 const HomePage = () => {
     return (
         <>
